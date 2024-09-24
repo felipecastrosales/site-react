c81dd91b30dae5865a9c9d2c394b7e6a5fb1dbfe
Felipe Sales <soufeliposales@gmail.com>
2024-09-23 22:18:43 -0300

    feat: add shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import { Button } from "@/components/ui/button"
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -25,6 +27,9 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+
+      <Button>Click me</Button>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
