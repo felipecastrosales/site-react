74fef444548ad347bd87aa05fbb201858d09fd97
Felipe Sales <soufeliposales@gmail.com>
2024-09-25 21:40:41 -0300

    feat: enhanced appbar

--- src/App.tsx
+++ src/App.tsx
@@ -21,6 +21,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h1>Vite + React | Vite + React | Vite + React | Vite + React | Vite + React | Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
