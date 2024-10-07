28d1aa91f71d22f934a93eedcef87a7876cd5e4a
Felipe Sales <soufeliposales@gmail.com>
2024-10-06 21:39:20 -0300

    feat: init to add components related to flow

---
+++ src/components/Content/Message/Message.tsx
@@ -0,0 +1,13 @@
+const SupportMessage = () => {
+  return (
+    <div style={{ textAlign: 'left', paddingBottom: '1rem' }}>
+      <p>
+        Se você está interessado em adquirir alguns produtos que eu recomendo e utilizo,
+        considere fazer a compra através dos links abaixo. Comprando pelo link será uma
+        maneira de me apoiar. Agradeço pelo seu apoio!
+      </p>
+    </div>
+  );
+};
+
+export default SupportMessage;
---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,9 @@
+import { Input } from "@/components/ui/input"
+
+const SearchComponent = () => {
+  return (
+    <Input placeholder="Pesquise pelo nome ou categoria do produto" />
+  );
+};
+
+export default SearchComponent;
