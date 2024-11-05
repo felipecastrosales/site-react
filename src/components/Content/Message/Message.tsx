8ccb1a0509d0a33c78d94e2003d6580ba24e4d94
Felipe Sales <soufeliposales@gmail.com>
2024-11-04 21:34:59 -0300

    chore: add component as react

--- src/components/Content/Message/Message.tsx
+++ src/components/Content/Message/Message.tsx
@@ -1,10 +1,12 @@
+import './Message.css';
+
 const SupportMessage = () => {
   return (
-    <div style={{ textAlign: 'left', paddingBottom: '1rem', fontSize: '1rem' }}>
+    <div className="support-message">
       <p>
-        Se você está interessado em adquirir alguns produtos que eu recomendo e utilizo,
-        considere fazer a compra através dos links abaixo. Comprando pelo link será uma
-        maneira de me apoiar. Agradeço pelo seu apoio!
+      Se você está interessado em adquirir alguns produtos que eu recomendo e utilizo,
+      considere fazer a compra através dos links abaixo. Comprando pelo link será uma
+      maneira de me apoiar. Agradeço pelo seu apoio!
       </p>
     </div>
   );
